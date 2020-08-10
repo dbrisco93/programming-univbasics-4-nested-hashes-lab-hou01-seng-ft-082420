0114f32844f8764b0dff21acf129d0ea14bedcb1
Dominique Brisco <dbrisco93@gmail.com>
2020-08-10 08:39:27 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,8 +83,8 @@ def adding_matz
         :languages => ["C"]
       }
     }
-    
-    adding_matz = programmer_hash[:yukihiro_matsumoto]
+    adding_matz = {known_for: "Ruby", languages: ["LISP", "C"]}
+    programmer_hash[:yukihiro_matsumoto] = adding_matz
     
 
 end

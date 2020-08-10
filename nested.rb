1ba977763ba12785fb136569058722135e415acf
Dominique Brisco <dbrisco93@gmail.com>
2020-08-10 08:54:53 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -129,6 +129,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-    adding_to_dennis = programmer_hash[languages].push("Assembly")
-    puts programmer_hash
+    adding_to_dennis = programmer_hash[:dennis_ritchie][:languages] << "Assembly")
+    programmer_hash
   end
